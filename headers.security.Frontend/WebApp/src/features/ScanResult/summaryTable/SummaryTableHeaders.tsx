febe1698df843dc476d47d3936835900f03b0c6c
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2024-04-08 13:48:15 +0200

    Filter handlers for header

--- headers.security.Frontend/WebApp/src/features/ScanResult/summaryTable/SummaryTableHeaders.tsx
+++ headers.security.Frontend/WebApp/src/features/ScanResult/summaryTable/SummaryTableHeaders.tsx
@@ -1,6 +1,6 @@
 import { List, ListDivider, ListItem, ListItemContent, ListItemDecorator, Typography } from '@mui/joy';
 import { ImpactIcon } from '../../../components/ImpactIcon.tsx';
-import { ISecurityConceptResult, ServerResult } from '../../../contracts/apiTypes.ts';
+import { ISecurityConceptResult, SecurityConceptHandlerName, ServerResult } from '../../../contracts/apiTypes.ts';
 
 interface SummaryTableHeadersItemProps {
   handlerResult: ISecurityConceptResult;
@@ -25,10 +25,25 @@ const SummaryTableHeadersItem = ({ handlerResult, last }: SummaryTableHeadersIte
   );
 };
 
+const hiddenHandlers = [SecurityConceptHandlerName.Server];
+
 export const SummaryTableHeaders = ({ data }: { data: ServerResult }) => {
-  // TODO: maybe need to separate certain handlers in backend already...
-  const headers = data.result.handlerResults.filter((h) => h.impact != 'Info');
+  const headers = data.result.handlerResults.filter((h) => {
+    // Remove explicitly hidden handlers
+    if (hiddenHandlers.includes(h.handlerName)) {
+      return false;
+    }
+
+    // Remove handlers showing an info impact, ensure all with a non-zero impact are always shown
+    if (h.impact == 'Info') return false;
+    if (h.impact != 'None') return true;
+
+    // Remove handlers with no impact and no processed value
+    return h.processedValue != null;
+  });
+
   const last = headers.length - 1;
+
   return (
     <List orientation="horizontal" wrap={true} size="sm" sx={{ marginLeft: '-0.5em', marginTop: '-0.1em' }}>
       {headers.map((handlerResult, idx) => (
