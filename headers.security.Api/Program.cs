72e948a7e1dc62ebc262d0c2b07427887ea27f61
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2024-04-09 13:51:32 +0200

    Don't use HTTPS redirection in code, handled by reverse proxy

--- headers.security.Api/Program.cs
+++ headers.security.Api/Program.cs
@@ -61,11 +61,9 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseRouting();
 
 app.MapControllers().RequireRateLimiting("fixed");
