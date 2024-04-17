512f20ebb25963eae55bc38130fdfa53bfcb97c8
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2024-04-17 11:12:17 +0200

    Improve CSP bypass matching and add testing

--- headers.security.Common/Constants/KnownCspBypassUris.cs
+++ headers.security.Common/Constants/KnownCspBypassUris.cs
@@ -5,27 +5,27 @@ namespace headers.security.Common.Constants;
 public static partial class KnownCspBypassUris
 {
     // TODO: FUTURE: might be better to use full URI here and do more advanced matching, e.g. query parameters can come in any order, this is good enough for now
-    private static readonly List<string> All = [
+    public static readonly List<string> All = [
         "google.com/complete/search",
         "youtube.com/oembed"
     ];
 
     public static bool Matches(string token)
     {
-        if (token == "*")
+        var cleanedToken = CleanedToken(token);
+        
+        if (cleanedToken == "*")
         {
             return true;
         }
-        
-        var cleanedToken = CleanedToken(token);
 
         return All.Any(bypassToken => cleanedToken.StartsWith("*.")
-            ? bypassToken.EndsWith(cleanedToken[2..])
+            ? bypassToken.StartsWith(cleanedToken[2..]) || bypassToken.Split('.').Last().StartsWith(cleanedToken[2..])
             : bypassToken.StartsWith(cleanedToken));
     }
     
-    private static string CleanedToken(string token) => ProtocolRegex().Replace(token, "").ToLowerInvariant();
+    private static string CleanedToken(string token) => ProtocolRegex().Replace(token, "", 1).ToLowerInvariant();
     
-    [GeneratedRegex(@"http[s]{0,1}:\/\/")]
+    [GeneratedRegex(@"^http[s]{0,1}:\/\/", RegexOptions.IgnoreCase)]
     private static partial Regex ProtocolRegex();
 }
---
+++ headers.security.Tests/CspBypassUriTests.cs
@@ -0,0 +1,65 @@
+using System.Diagnostics.CodeAnalysis;
+using headers.security.Common.Constants;
+using headers.security.Scanner.SecurityConcepts.Csp;
+
+namespace headers.security.Tests;
+
+[SuppressMessage("ReSharper", "InvokeAsExtensionMethod")]
+public class CspBypassUriTests
+{
+    public static IEnumerable<object[]> AllKnownBypassUris() => KnownCspBypassUris.All
+        .Select(verbatimUri => (object[])[verbatimUri]);
+
+    [Theory]
+    [MemberData(nameof(AllKnownBypassUris))]
+    public void MatchesKnownBypassUrisExactly(string token)
+    {
+        KnownCspBypassUris.Matches(token).Should().BeTrue();
+    }
+
+    [Theory]
+    [MemberData(nameof(AllKnownBypassUris))]
+    public void MatchesPartialKnownBypassUris(string token)
+    {
+        var domainToken = token.Split('/').First();
+        KnownCspBypassUris.Matches(domainToken).Should().BeTrue();
+    }
+
+    [Theory]
+    [MemberData(nameof(AllKnownBypassUris))]
+    public void MatchesWildcardKnownBypassUris(string token)
+    {
+        var wildcardToken = "*." + token.Split('.').Last();
+        KnownCspBypassUris.Matches(wildcardToken).Should().BeTrue();
+        
+        var wildcardFullToken = "*." + token;
+        KnownCspBypassUris.Matches(wildcardFullToken).Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("example.com")]
+    [InlineData("http://example.com")]
+    [InlineData("https://example.com")]
+    [InlineData("HTTPS://example.com")]
+    [InlineData("le.com")]
+    [InlineData("*.le.com")]
+    [InlineData("safe.google.com")]
+    [InlineData("*.safe.google.com")]
+    public void DoesNotMatchSafeTarget(string token)
+    {
+        KnownCspBypassUris.Matches(token).Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData("*")]
+    [InlineData("http://*")]
+    [InlineData("https://*")]
+    [InlineData("HTTPS://*")]
+    [InlineData("*.com")]
+    [InlineData("google.com")]
+    [InlineData("*.google.com")]
+    public void MatchesKnownBadTargets(string token)
+    {
+        KnownCspBypassUris.Matches(token).Should().BeTrue();
+    }
+}
