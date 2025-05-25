5e40456b8eae14abb33eb0a03b2574156b40f644
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2025-05-25 17:27:44 +0200

    Fix Azure CloudLookupClientProvider to work with current Microsoft download portal behaviour

--- CloudLookup/Providers/AzureCloudLookupClientProvider.cs
+++ CloudLookup/Providers/AzureCloudLookupClientProvider.cs
@@ -12,13 +12,20 @@ public partial class AzureCloudLookupClientProvider(IHttpClientFactory httpClien
     {
         var downloadPageHtml = await FetchContent(_downloadPage);
 
-        var urlMatch = DataUrlRe().Match(downloadPageHtml);
+        var matches = DataUrlRe().Matches(downloadPageHtml);
+        var urls = matches
+            .Select(match => Uri.TryCreate(match.Groups["URI"].Value, UriKind.Absolute, out var uri) 
+                ? uri 
+                : null)
+            .Where(u => u != null)
+            .ToList();
 
-        if (!urlMatch.Success || !Uri.TryCreate(urlMatch.Groups["URI"].Value, UriKind.Absolute, out var contentUri))
+        if (!urls.Any())
         {
             throw new ArgumentException("Failed to locate Azure IP ranges JSON URI.");
         }
 
+        var contentUri = GetLatest(urls);
         var content = await FetchContent(contentUri);
 
         var tokens = GenericJsonCloudLookupClientProvider.ExtractTokens(content);
@@ -26,8 +33,25 @@ public partial class AzureCloudLookupClientProvider(IHttpClientFactory httpClien
         return ExtractNetworks(tokens);
     }
 
+    private Uri GetLatest(List<Uri> uris)
+    {
+        var latest = uris.First();
+        foreach (var uri in uris.Skip(1))
+        {
+            if (IsNewer(uri, latest))
+            {
+                latest = uri;
+            } 
+            
+        }
+
+        return latest;
+    }
+
+    private bool IsNewer(Uri a, Uri b) => string.Compare(a.Segments.Last(), b.Segments.Last(), StringComparison.OrdinalIgnoreCase) > 0;
+
     [GeneratedRegex(
-        @"href=""(?<URI>https?://download\.microsoft\.com/[\w\d/\-_]+\.json)""",
+        @"""(?<URI>https?://download\.microsoft\.com/[\w\d/\-_]+\.json)""",
         RegexOptions.NonBacktracking | RegexOptions.ExplicitCapture | RegexOptions.IgnoreCase
     )]
     private static partial Regex DataUrlRe();
